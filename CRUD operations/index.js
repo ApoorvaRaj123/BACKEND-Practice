7f9f1039410394fcdb9586da6bc068fed2783eed
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-08-10 21:56:35 +0530

    Apply CRUD & routes

--- CRUD operations/index.js
+++ CRUD operations/index.js
@@ -1,11 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+const userModel = require("./usermodel.js");
 
-app.get("/", function(req, res){
-    res.send("Started");    
+app.get("/", function (req, res) {
+  res.send("Started");
 });
 
+app.get("/creat", async (req, res) => {
+  let createduser = await userModel.create({
+    name: "haha",
+    username: "jojo",
+    email: "hoho@gmail.com",
+  });
+  res.send(createduser);
+});
+
+app.get("/updat", async (req, res) => {
+  let updateduser = await userModel.findOneAndUpdate(
+    { name: "haha" },
+    { username: "hehe" },
+    { new: true }
+  );
+  res.send(updateduser);
+});
+
+app.get("/read", async (req, res) => {
+   let users = await userModel.find()
+   res.send(users);
+})
 
 
 app.listen(3000);
