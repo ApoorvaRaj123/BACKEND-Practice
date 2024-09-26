4cdfcad1115da4bdeb1db3d77cbb53d6fc0dda85
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-09-26 21:39:00 +0530

    Update app.js

--- miniProject_dataAssociation/app.js
+++ miniProject_dataAssociation/app.js
@@ -16,6 +16,14 @@ app.get("/", (req,res)=>{
     res.render('index');
 })
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
 app.post("/register", async (req,res)=>{
     let {name, username, email, password, age} = req.body;
     let user = await userModel.findOne({email});
@@ -40,5 +48,33 @@ app.post("/register", async (req,res)=>{
     })
 })
 
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send("Something went wrong");
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      res.status(200).send("Logged in");
+      let token = jwt.sign({ email: email, userid: createdUser._id }, "shhhh");
+      res.cookie("token", token);
+    } else res.redirect("/login");
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.render("login");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token == "") res.send("Must be Logged in");
+  else {
+    let data = jwt.verify(req.cookies.token, "shhh");
+    req.user = data;
+  }
+  next();
+}
 
 app.listen(3000);
